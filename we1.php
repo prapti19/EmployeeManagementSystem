29083a92c30229e53bd740bb41833ecbb741c9cc
purva98 <purvamhasakar@gmail.com>
2019-10-13 12:42:16 +0530

    Updated we1.php

--- we1.php
+++ we1.php
@@ -7,6 +7,7 @@ include('connsp.php');
 ?>
 <head>
   <title>Resign Applicants</title>
+  <title>Resign applications will be visible to the manager</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 <!-- Optional theme -->
